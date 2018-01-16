a5037ca1b2094f2b2c4329baaeb4dfb618e126ea
YueLin <contact@lylares.com>
2018-01-16 22:00:55 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,14 +6,11 @@
 
 <footer data-am-widget="footer" class="am-footer am-footer-default " data-am-footer="{  }" id="pg">
         <div class="am-footer-miscs">
-            <p>由 <span class="am-icon-skyatlas "></span><a href="https://lylares.com/" title="LYLARES'S BLOG" target="_blank" class="">LYLARES'S BLOG</a>
-            提供技术支持</p>
-            <p>© XXXX  ALL RIGHTS RESERVED.</p>
             <p>备案号</p>
         </div>
 </footer>
 <!-- 滚动加载插件 -->
-<script src="https://static.lylares.com/wd/assets/js/jquery.lazyload.min.js?v1.0"></script>
-<script src="https://static.lylares.com/wd/assets/js/amazeui.min.js"></script>
+<script src="https://lab.lylares.com/vip/js/jquery.lazyload.min.js?v1.0"></script>
+<script src="https://lab.lylares.com/assets/js/amazeui.min.js"></script>
 </body>
 </html>
