04a9e8f9addd6c530aec1c7ce46c55715a743f9b
YueLin <contact@lylares.com>
2018-04-04 20:52:51 +0800

    update
    update

--- footer.php
+++ footer.php
@@ -6,11 +6,18 @@
 
 <footer data-am-widget="footer" class="am-footer am-footer-default " data-am-footer="{  }" id="pg">
         <div class="am-footer-miscs">
-            <p>备案号</p>
+            <p>© <?php echo date('Y');?> <? echo $webConfig['copyright'];?></p>
+            <p><? echo $webConfig['beian']; ?></p>
         </div>
 </footer>
+<!-- layer弹窗插件 -->  
+<script src="./static/layer/layer.js"></script>
 <!-- 滚动加载插件 -->
-<script src="https://lab.lylares.com/vip/js/jquery.lazyload.min.js?v1.0"></script>
-<script src="https://lab.lylares.com/assets/js/amazeui.min.js"></script>
+<script src="./static/js/jquery.lazyload.min.js?v1.0"></script>
+
+<script type="text/javascript">
+    // 保留版权可好
+console.info('来自LYLARES\'S BLOG，Version 1.2, 目前整站已经开源，请参考https://www.lylares.com/the-new-site-of-bing-everyday-gallery.html Designed by www.lylares.com.');	
+</script>
 </body>
 </html>
