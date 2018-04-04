59ac9805619cfb3e815ce1970a12710cfa64252a
YueLin <contact@lylares.com>
2018-04-04 20:53:39 +0800

    update code
    update code

--- con_qn.php
+++ con_qn.php
@@ -12,28 +12,4 @@ $prefix = '';
 $marker = '';
 // 本次列举的条目数
 $delimiter = '/';
-// 列举文件
 list($ret, $err) = $bucketManager->listFiles($bucket, $prefix, $marker, $limit, $delimiter);
-if ($err !== null) {
-   // echo "\n====> list file err: \n";
-   // var_dump($err);
-} else {
-   if (array_key_exists('marker', $ret)) {
-       // echo "Marker:" . $ret["marker"] . "\n";
-   }  
-$arr=array_reverse($ret['items']);
-   function getParam($key,$default='')
-{
-    return trim($key && is_string($key) ? (isset($_POST[$key]) ? $_POST[$key] : (isset($_GET[$key]) ? $_GET[$key] : $default)) : $default);
-}
-$p = intval(getParam('p', '1')); 
-if($p == '') $p = '1';
-$pg=$p-1;
-if (count($arr)%9 ==0 ){
-	$maxPage=count($arr)/9; 
-}
-$maxPage=ceil((count($arr)+1)/9); 
-$num=count($arr)/9;
-$num=explode(".",$num);
-$num=substr($num[1],1,1);
-}
